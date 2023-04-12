cd182b77dbbf76bf796e3b7150d40486653fb1cd
XanderRubio <clemens.alex7@gmail.com>
2023-04-11 19:09:16 -0500

    Refactoring data

--- src/components/ListCast.jsx
+++ src/components/ListCast.jsx
@@ -1,17 +1,6 @@
-import { useState, useEffect } from "react";
-
-export default ({onChoice}) => {
+export default ({cast, onChoice}) => {
   const [cast, setCast] = useState([]);
-
-  async function fetchCast() {
-    const response = await fetch('cast.json');
-    setCast(await response.json());
-  }
-
-  useEffect(() => {
-    fetchCast();
-  });
-
+  
   return (
     <div style={{
       display: "grid",
